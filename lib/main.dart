f356043d885cde75ccf345e5bacdd28d9c8e1517
ehteshamawan1 <ehtesham.awan.ea@gmail.com>
2023-06-09 18:07:29 +0500

    committed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:multi_role_base_app/screens/home_screen.dart';
+import 'package:multi_role_base_app/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomeScreen(),
+      home: SplashScreen(),
     );
   }
 }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () async {},
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                color: Colors.indigo,
+                child: const Center(
+                  child: Text('Click'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

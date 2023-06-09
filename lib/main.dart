54c58a08237b544091e0d12a6e15e95130628eb0
ehteshamawan1 <ehtesham.awan.ea@gmail.com>
2023-06-09 19:14:13 +0500

    committed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:multi_role_base_app/screens/login_screen.dart';
+import 'package:multi_role_base_app/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: LoginScreen(),
+      home: SplashScreen(),
     );
   }
 }
